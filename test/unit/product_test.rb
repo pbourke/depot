8e74c3162ef08b12bb37f2187e3267339e7e4032
Patrick Bourke <pbourke@gmail.com>
2011-07-15 09:17:37 -0700

    Finished Iteration B2 in book

--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -1,6 +1,13 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
+  def new_product(image_url)
+    Product.new(:title       => "Some Title",
+                :description => "xyz",
+                :price       => 1,
+                :image_url   => image_url);
+  end
+
   test "empty attributes should not pass validation" do
     product = Product.new
     assert product.invalid?
@@ -11,7 +18,7 @@ class ProductTest < ActiveSupport::TestCase
   end
   
   test "price must be positive" do
-    product = Product.new(:title => "test",
+    product = Product.new(:title => "test title which should validate length",
                           :description => "foobar",
                           :image_url => "xyz.jpg")
     product.price = -1
@@ -26,5 +33,31 @@ class ProductTest < ActiveSupport::TestCase
 
     product.price = 1
     assert product.valid?
+  end
+
+  test "image url is validated" do
+    valid_urls = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg http://a.b.c/x/y/z/Fred.gif }
+    invalid_urls = %w{ fred.doc fred.gif/more fred.jpeg fred.gif.more }
+
+    valid_urls.each do |url|
+      assert new_product(url).valid?, "#{url} should be valid"
+    end
+
+    invalid_urls.each do |url|
+      assert new_product(url).invalid?, "#{url} should not be valid"
+    end
+  end
+
+  test "titles must be unique" do
+    product = new_product("some_url.jpg")
+    product.title = products(:bacon).title
+    assert !product.save
+    assert_equal I18n.translate("activerecord.errors.messages.taken"), product.errors[:title].join('; ');
+  end
+
+  test "no short titles" do
+    product = new_product("some_url.jpg")
+    product.title = "small"
+    assert !product.save
   end
 end
